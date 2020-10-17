f80c15f8bfe096b740f41ce3ab1c9943d110403d
zhang <2873908996@qq.com>
2020-10-17 17:25:17 +0800

    bug fix: 项目改为普通的mavan项目

--- src/main/java/designdemo/factory/abstractfactory/Main.java
+++ src/main/java/designdemo/factory/abstractfactory/Main.java
@@ -4,6 +4,7 @@ import designdemo.factory.abstractfactory.abstractclass.AbstractFactory;
 import designdemo.factory.abstractfactory.abstractclass.Food;
 import designdemo.factory.abstractfactory.abstractclass.Gun;
 import designdemo.factory.abstractfactory.abstractclass.Transportion;
+import designdemo.factory.abstractfactory.ukclass.UkFactory;
 import designdemo.factory.abstractfactory.usaclass.UsaFactory;
 
 /**
@@ -13,9 +14,9 @@ import designdemo.factory.abstractfactory.usaclass.UsaFactory;
 public class Main {
     public static void main(String[] args) {
         //当前使用Uas的族类工厂
-        AbstractFactory factory = new UsaFactory();
+        //AbstractFactory factory = new UsaFactory();
         //想变成Uk的只需要修改工厂实体就行
-        //AbstractFactory factory = new UkFactory();
+        AbstractFactory factory = new UkFactory();
 
         //下面的代码不会修改
         Food food = factory.createFood();
