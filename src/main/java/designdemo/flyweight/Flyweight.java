71bbc62557c51ffd9739df2333d403f456b7fcd2
zhang <2873908996@qq.com>
2020-10-26 17:51:21 +0800

    add fix: flyweight代码实现

---
+++ src/main/java/designdemo/flyweight/Flyweight.java
@@ -0,0 +1,10 @@
+package designdemo.flyweight;
+
+/**
+ * Author : GuDao
+ * 2020-10-26
+ */
+
+public abstract class Flyweight {
+    abstract void operation(UnSpecoficFlyweight unSpecoficFlyweight);
+}
